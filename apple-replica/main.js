1eb868ee708ebf18d8a0eac1bdac27e0356d8177
Nicholas Seegobin <Nickysterling@rogers.com>
2021-04-07 15:27:15 -0400

    Initial commit of the Apple replica website

---
+++ apple-replica/main.js
@@ -0,0 +1,9 @@
+//JavaScript
+const selectElement = (element) => document.querySelector(element);
+
+selectElement('.mobile-menu').addEventListener('click', () => {
+    selectElement('header').classList.toggle('active');
+});
+
+
+
